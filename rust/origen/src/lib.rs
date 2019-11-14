abb416059f5de9e5f6d201bd92300a2a2e3c2fa2
Stephen McGinty <stephen.f.mcginty@gmail.com>
2019-11-14 22:46:43 +0000

    Python ext now building

--- rust/origen/src/lib.rs
+++ rust/origen/src/lib.rs
@@ -1,3 +1,5 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 #[macro_use]
 extern crate lazy_static;
 
@@ -5,4 +7,17 @@ pub mod workspace;
 
 lazy_static! {
     pub static ref CONFIG: workspace::Config = workspace::Config::default();
+}
+
+#[pyfunction]
+fn hello() -> PyResult<String> {
+    Ok("Yo!".to_string())
+}
+
+/// This module is a python module implemented in Rust.
+#[pymodule]
+fn origen(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(hello))?;
+
+    Ok(())
 }
