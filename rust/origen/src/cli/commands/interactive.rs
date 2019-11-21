5f7cfceedca370c4a3337f364d22c28b8def498e
Stephen McGinty <stephen.f.mcginty@gmail.com>
2019-11-21 13:10:33 +0000

    Make dut and tester available to console

--- rust/origen/src/cli/commands/interactive.rs
+++ rust/origen/src/cli/commands/interactive.rs
@@ -5,6 +5,7 @@ pub fn main() {
         "
 import origen;
 import code;
+from origen import dut, tester;
 code.interact(banner=f\"Origen {origen.version}\", local=locals(), exitmsg=\"\")
     ",
     );
