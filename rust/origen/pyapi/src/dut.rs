112604d5e2d3a3febb90781968a8542e8cb4f021
Stephen McGinty <stephen.f.mcginty@gmail.com>
2019-12-12 16:10:31 +0000

    Fixed error conversion to Py

--- rust/origen/pyapi/src/dut.rs
+++ rust/origen/pyapi/src/dut.rs
@@ -2,7 +2,6 @@ use pyo3::prelude::*;
 //use pyo3::wrap_pyfunction;
 use crate::register::BitCollection;
 use origen::DUT;
-use pyo3::exceptions;
 
 /// Implements the module _origen.dut in Python which exposes all
 /// DUT-related APIs
@@ -28,12 +27,7 @@ impl PyDUT {
 
     /// Creates a new model at the given path
     fn create_sub_block(&self, path: &str, id: &str) -> PyResult<()> {
-        DUT.lock()
-            .unwrap()
-            .create_sub_block(path, id)
-            // Can't get the Origen errors to cast properly to a PyErr For some reason,
-            // so have to do this
-            .map_err(|e| exceptions::OSError::py_err(e.msg))
+        Ok(DUT.lock().unwrap().create_sub_block(path, id)?)
     }
 
     fn create_reg(
@@ -46,27 +40,14 @@ impl PyDUT {
         size: Option<u32>,
     ) -> PyResult<()> {
         let mut dut = DUT.lock().unwrap();
-        // Can't get the Origen errors to cast properly to a PyErr For some reason,
-        // so have to do this
-        let model = match dut.get_mut_model(path) {
-            Ok(m) => m,
-            Err(e) => return Err(exceptions::OSError::py_err(e.msg)),
-        };
-        model
-            .create_reg(memory_map, address_block, id, offset, size)
-            // Can't get the Origen errors to cast properly to a PyErr For some reason,
-            // so have to do this
-            .map_err(|e| exceptions::OSError::py_err(e.msg))
+        Ok(dut
+            .get_mut_model(path)?
+            .create_reg(memory_map, address_block, id, offset, size)?)
     }
 
     fn number_of_regs(&self, path: &str) -> PyResult<usize> {
         let dut = DUT.lock().unwrap();
-        // Can't get the Origen errors to cast properly to a PyErr For some reason,
-        // so have to do this
-        let model = match dut.get_model(path) {
-            Ok(m) => m,
-            Err(e) => return Err(exceptions::OSError::py_err(e.msg)),
-        };
+        let model = dut.get_model(path)?;
         Ok(model.number_of_regs())
     }
 
@@ -78,14 +59,8 @@ impl PyDUT {
         id: &str,
     ) -> PyResult<BitCollection> {
         let dut = DUT.lock().unwrap();
-        let model = match dut.get_model(path) {
-            Ok(m) => m,
-            Err(e) => return Err(exceptions::OSError::py_err(e.msg)),
-        };
-        let reg = match model.get_reg(memory_map, address_block, id) {
-            Ok(m) => m,
-            Err(e) => return Err(exceptions::OSError::py_err(e.msg)),
-        };
+        let model = dut.get_model(path)?;
+        let reg = model.get_reg(memory_map, address_block, id)?;
 
         Ok(BitCollection::from_reg(
             path,
--- rust/origen/pyapi/src/memory_map.rs
+++ rust/origen/pyapi/src/memory_map.rs
@@ -3,7 +3,6 @@ use crate::dut::PyDUT;
 use origen::DUT;
 use pyo3::class::basic::PyObjectProtocol;
 use pyo3::class::PyMappingProtocol;
-use pyo3::exceptions;
 use pyo3::prelude::*;
 
 /// Implements the user APIs dut[.sub_block].memory_map() and
@@ -12,11 +11,8 @@ use pyo3::prelude::*;
 impl PyDUT {
     fn memory_maps(&self, path: &str) -> PyResult<MemoryMaps> {
         let dut = DUT.lock().unwrap();
-        // Verify this model exists, though we don't need it for now
-        match dut.get_model(path) {
-            Ok(m) => m,
-            Err(e) => return Err(exceptions::OSError::py_err(e.msg)),
-        };
+        // Verify the model exists, though we don't need it for now
+        dut.get_model(path)?;
         Ok(MemoryMaps {
             model_path: path.to_string(),
         })
@@ -32,18 +28,18 @@ pub struct MemoryMaps {
     model_path: String,
 }
 
+/// User API methods, available to both Rust and Python
 #[pymethods]
 impl MemoryMaps {
     fn len(&self) -> PyResult<usize> {
         let dut = DUT.lock().unwrap();
-        // Verify this model exists, though we don't need it for now
-        let model = match dut.get_model(&self.model_path) {
-            Ok(m) => m,
-            Err(e) => return Err(exceptions::OSError::py_err(e.msg)),
-        };
+        let model = dut.get_model(&self.model_path)?;
         Ok(model.memory_maps.len())
     }
 }
+
+/// Internal, Rust-only methods
+impl MemoryMaps {}
 
 #[pyproto]
 impl PyMappingProtocol for MemoryMaps {
