af4930b3fe3bbb5b36fced6b1c967839211e5c91
Paul DeRouen <paul.derouen@nxp.com>
2019-12-19 07:25:59 -0600

    added comments for discussion

--- rust/origen/src/core/pattern/action.rs
+++ rust/origen/src/core/pattern/action.rs
@@ -1,5 +1,9 @@
 //! Defines the set of actions associated with pattern generation
-// TODO: Pick a better type for data integers will not suffice for very large registers or banks of pins
+// TODO: Pick a better type for data. Integers will not suffice for very large registers or banks of pins
+// Enum Structs will be defined for most action types to hold their associated info
+//
+// I envision the internal pattern storage type being a vector of type Pattern::Action
+// Code processing the Pattern will use match with arms to process the type of Actions that are supported for the given output
 
 use super::operation::Operation;
 use super::pinaction::PinAction;
@@ -8,7 +12,7 @@ pub enum Action {
     Pin(PinAction),
     Timeset{name: String},
     Cycle{repeat: u32},
-    // likely need a larger storage type for address and data
-    Register{name: String, address: u32, data: u32, operation: Operation, start_stop: Operation},
-    Driver{name: String, operation: Operation, data: u32, size: u32, target: String, start_stop: Operation},
+    // likely need a larger storage type for address and data, or maybe generics to provide options
+    // Register{name: String, address: u32, data: u32, operation: Operation, start_stop: Operation},
+    // Driver{name: String, operation: Operation, data: u32, size: u32, target: String, start_stop: Operation},
 }
