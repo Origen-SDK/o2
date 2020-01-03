3923d7e85020d5e5680f9504ee2d903713a81243
info-rchitect <info-rchitect@D10-brian.amd.com>
2020-01-03 09:20:10 -0600

    removed old comment

--- example/tests/translator_test.py
+++ example/tests/translator_test.py
@@ -9,7 +9,6 @@ def test_translator_ip_xact():
     # TODO: Must be a better way to check this
     assert str(type(origen.dut.memory_map("RegisterMap"))) == "<class 'MemoryMap'>"
     assert origen.dut.memory_map("RegisterMap").regs.len() == 2
-    # TODO: Enable the test below when it is deterministic (https://github.com/Origen-SDK/o2/issues/47)
     assert origen.dut.memory_map("RegisterMap").regs.keys() == ['dut_top_level_reg','dut_top_level_reg_number_two']
     # TODO: Cannot test register attributes currently due to this issue:
     # (Pdb) origen.dut.memory_map("RegisterMap").regs('dut_top_level_reg_number_two')
