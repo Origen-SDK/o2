ddf5e38e6523e8c3783a3d46b7bf5410179efa25
info-rchitect <info-rchitect@D10-brian.amd.com>
2020-01-07 10:39:54 -0600

    battle hardened error code

--- example/tests/errors_test.py
+++ example/tests/errors_test.py
@@ -11,3 +11,11 @@ def test_duplicate_instance_error():
     with pytest.raises(DuplicateInstanceError) as err_info:
         origen.dut.add_sub_block('core0')
     assert str(err_info.value) == "Cannot create instance of 'sub_block' named 'core0', it already exists!"
+    # Check the default error message
+    with pytest.raises(DuplicateInstanceError) as err_info:
+        raise DuplicateInstanceError('test_str')
+    assert str(err_info.value) == "Cannot create instance of 'str', it already exists!"
+    # Make sure users know the class alias argument must be a string
+    with pytest.raises(TypeError) as err_info:
+        raise DuplicateInstanceError('test_str', [1])
+    assert str(err_info.value) == "Class alias must be of type 'str'!"
