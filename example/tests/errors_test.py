7080e0f76c46d17e218b9608a8e986be05b1b8ef
info-rchitect <info-rchitect@D10-brian.amd.com>
2020-01-07 09:54:04 -0600

    changed name of test to match exception name

--- example/tests/errors_test.py
+++ example/tests/errors_test.py
@@ -5,7 +5,7 @@ from origen.errors import *
 def boot_falcon():
     return origen.app.instantiate_dut("dut.falcon") if origen.dut is None else origen.dut
 
-def test_duplicate_detected_error():
+def test_duplicate_instance_error():
     boot_falcon()
     assert list(origen.dut.sub_blocks.keys()) == ['core0', 'core1', 'core2', 'core3']
     with pytest.raises(DuplicateInstanceError) as err_info:
