edd6fd2394c65336dac9105dc40745ef9cec4057
info-rchitect <info-rchitect@D10-brian.amd.com>
2020-01-15 14:27:08 -0600

    added more tests

--- example/tests/compiler_test.py
+++ example/tests/compiler_test.py
@@ -42,5 +42,6 @@ def test_compiler_templates_files():
     assert bool(compiled_file_status.st_mode & stat.S_IRUSR) == True
     assert bool(compiled_file_status.st_mode & stat.S_IWUSR) == True
     assert bool(compiled_file_status.st_mode & stat.S_IWUSR) == True
+    assert compiled_file_status.st_size == 33
 
    
