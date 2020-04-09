6f78bdf85bda8c1ed3db83574e3412b6edce1377
Stephen McGinty <stephen.f.mcginty@gmail.com>
2020-04-09 13:50:33 +0100

    Travis deploy test

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,27 +3,29 @@
 set -ex
 
 main() {
-    src=$(pwd)
-    stage=$(mktemp -d)
-
-    test -f Cargo.lock || cargo generate-lockfile
-
     if [ "$TRAVIS_OS_NAME" = "windows" ]; then
         export PATH="/c/PythonForO2:/c/PythonForO2/Scripts:$PATH"
     else
         source /home/travis/virtualenv/python$PYTHON_VERSION/bin/activate
     fi
-    
+
     cd origen
+
+    src=$(pwd)
+    stage=$(mktemp -d)
+
+    test -f Cargo.lock || cargo generate-lockfile
+    
     cargo build --target $TARGET --release --workspace --bins
     cp target/$TARGET/release/origen $stage/
-    cd ../
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
     cd $src
 
     rm -rf $stage
+
+    cd ../
 }
 
 main
