c158bb0bc2b12e28372c1a28ca810e05fb7a8a95
Corey Engelken <corey.engelken@nxp.com>
2020-05-25 22:37:09 -0500

    Add more features to ose and add additional config

--- python/origen/web/sphinx_ext/sphinx.py
+++ python/origen/web/sphinx_ext/sphinx.py
@@ -1,6 +1,6 @@
 from sphinx.errors import ExtensionError
 from sphinx.util.logging import getLogger
-import origen, origen.web, shutil, copy, subprocess, pathlib
+import origen, origen.web, shutil, copy, subprocess, pathlib, builtins, types, inspect
 import sphinxbootstrap4theme
 from recommonmark.transform import AutoStructify
 
@@ -88,6 +88,8 @@ def setup(sphinx):
   sphinx.add_config_value("origen_subprojects", {}, '')
   sphinx.add_config_value("origen_no_api", None, 'env')
   sphinx.add_config_value("origen_templates", None, '')
+  sphinx.add_config_value("origen_api_module_data_clashes", {}, '')
+  sphinx.add_config_value("origen_refs_for", {}, '')
 
   sphinx.connect("config-inited", apply_origen_config)
   sphinx.connect("builder-inited", build_subprojects)
@@ -95,27 +97,27 @@ def setup(sphinx):
   sphinx.add_html_theme('origen', str(theme_dir))
   sphinx.config.html_theme = 'origen'
 
-  # Note: Origen includes the recommomark module, so even if the user removes it from the extensions list in their own config,
+  # Note: Origen includes the recommonmark module, so even if the user removes it from the extensions list in their own config,
   #  this will still be safe. It'll just have no usage.
   # Setup taken from: https://recommonmark.readthedocs.io/en/latest/auto_structify.html
   # Adding the config here so users get it for free - its not particularly obvious what this does so don't want to risk them messing with it.
-  # It can be overriden in the their own 'setup' method
+  # It can be overridden in the their own 'setup' method
   github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
   sphinx.add_config_value('recommonmark_config', {
             'url_resolver': lambda url: github_doc_root + url,
             'auto_toc_tree_section': 'Contents',
             }, True)
   sphinx.add_transform(AutoStructify)
-  sphinx.connect("source-read", jinja_integrator)
+  sphinx.connect("source-read", preprocess_src)
 
 '''
   The 'setup' method will be run before the user's config, allowing us to 'preconfigure' several
     items and make their config simpler, less error pronne, and, well, preconfigured.
-  The setup below is run AFTER their config was been run. We can take their settings/overrides and conmbine
+  The setup below is run AFTER their config was been run. We can take their settings/overrides and combine
     them with what we have here.
   
   Regarding theme options:
-    We'll set the 'html_theme' to 'origen' by default, but if its overriden then all our theme stuff is skipped.
+    We'll set the 'html_theme' to 'origen' by default, but if its overridden then all our theme stuff is skipped.
 '''
 def apply_origen_config(sphinx, config):
   if config.origen_no_api:
@@ -125,6 +127,14 @@ def apply_origen_config(sphinx, config):
       config.autoapi_modules.clear()
     if "autodoc_modules" in config.__dict__:
       config.autodoc_modules.clear()
+  sphinx.connect("autodoc-process-docstring", process_docstring)
+
+  if len(config.origen_api_module_data_clashes) > 0:
+    sphinx.connect('autoapi-process-node', no_index_clashes)
+    vars = []
+    for v in config.origen_api_module_data_clashes.values():
+      vars += [f'"{n}"' for n in v]
+    sphinx.config.html_context['origen_module_pydata_clashes_js'] = "[" + ', '.join(vars) + "]"
 
   # Theme specific setup - assuming Origen's theme is used (set by default, but overridable in their config)
   # Aside: if they set the theme to 'None', Sphinx's default (Alabastar) will be used
@@ -154,7 +164,10 @@ def apply_origen_config(sphinx, config):
     sphinx.add_css_file("https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/darkly/bootstrap.min.css")
     sphinx.add_css_file('sphinxbootstrap4.css')
     sphinx.add_css_file('https://gitcdn.link/repo/dracula/pygments/master/dracula.css') # ('dracula.css')
+    sphinx.add_css_file('quote_card.css')
     sphinx.add_css_file('origen.css')
+
+    sphinx.config.html_context['origen_version'] = origen.version
 
     # Merge the user's theme setup with Origen's
     if 'html_theme_options' in config:
@@ -188,22 +201,70 @@ def apply_origen_config(sphinx, config):
         **ORIGEN_THEME_DEFAULTS
       }
 
+def preprocess_src(app, docname, source):
+  # Origen's compiler supports multiple templating engines as well as a different context
+  # than Sphinx's Jinja engine.
+  # The Jinja compiler will be run on every source from Sphinx, unless the Origen compiler matches.
+  # In order to be detected from Origen though, the file must be appended with the template engine.
+  # For example:
+  #   index.rst -> Processed with Sphinx's Jinja interface
+  #   index.rst.mako -> Processed with Origen's mako interface
+  #   index.rst.jinja -> Processed with Origen's jinja interface
+  syntax = False # origen.compiler.select_syntax(docname)
+  if syntax:
+    source[0] = origen.compiler.render_string(source, syntax)
+  else:
+    jinja_integrator(app, docname, source)
+
 # Setup taken from here: https://www.ericholscher.com/blog/2016/jul/25/integrating-jinja-rst-sphinx/
 def jinja_integrator(app, docname, source):
   src = source[0]
   import builtins, types, inspect
-  rendered = app.builder.templates.render_string(
-    src, {
-      **builtins.__dict__,
-      **types.__dict__,
-      **inspect.__dict__,
-      **{
-        'origen': origen,
-        'origen_sphinx_ext': origen.web.sphinx_ext,
-      }
-    } # app.config.html_context
-  )
+  rendered = app.builder.templates.render_string(src, jinja_context(app))
   source[0] = rendered
+
+def jinja_render_string(app, src, additional_context={}):
+  return app.builder.templates.render_string(src, jinja_context(app))
+
+def jinja_context(app):
+  return {
+    **builtins.__dict__,
+    **types.__dict__,
+    **inspect.__dict__,
+    **{
+      'origen': origen,
+      'origen_sphinx_ext': origen.web.sphinx_ext,
+      'app': app,
+      'ref_for': lambda ref, txt=None, **opts : ref_for(app, ref, txt, **opts),
+      'path_to': lambda ref, **opts: path_to(app, ref, **opts),
+      'insert_cmd_output': lambda cmd, **opts: insert_cmd_output(app, cmd, **opts),
+    },
+    **app.config.html_context,
+  }
+
+def ref_for(app, ref, txt=None, **opts):
+  r = app.config.origen_refs_for[ref]
+  return f":ref:`{txt if txt else ref} <{r}>`"
+
+def path_to(app, ref, **opts):
+  return app.config.origen_refs_for[ref]
+
+def insert_cmd_output(app, cmd, **opts):
+  # Run the command and gather the output
+  out = subprocess.run(cmd, capture_output=True)
+  out = out.stdout.decode('utf-8').strip()
+
+  # Embed the output in a code block
+  # Need to also shift the spacing of the output so its all under the code block
+  # Also allow for the caller to place some prepend some additional spacing, in case this is used
+  #   inside another block
+  spacing = " " * opts['prepend_spaces'] if 'prepend_spaces' in opts else ""
+  retn = [f"{spacing}.. code:: none", ""]
+  retn += [f"{spacing}  {l}" for l in out.split("\n")]
+  return "\n".join(retn)
+
+def origen_bridge(app, docname, source):
+  ...
 
 '''
   Builds any Origen projects whose documentation should be built within
@@ -225,7 +286,7 @@ def compile():
   parameters to discern where the final output should be located on the
   site.
 
-  This can be overriden by supplying a difference function to the config
+  This can be overridden by supplying a difference function to the config
   value 'origen_deploy_function'
 '''
 def deploy():
@@ -239,3 +300,17 @@ def clean(partial_config):
   if hasattr(partial_config, 'origen_subprojects'):
     for subp, _config in partial_config.origen_subprojects.items():
       SubProject(subp, _config).clean()
+
+def no_index_clashes(app, node):
+  ''' Adds the :noindex: directive to matching nodes '''
+  if node.name in app.config.origen_api_module_data_clashes:
+    clashes = app.config.origen_api_module_data_clashes[node.name]
+    for clash in clashes:
+      node.variables[clash.split('.')[-1]][1]['directives'].append('noindex')
+
+def process_docstring(app, what, name, obj, options, lines):
+  ''' Runs the template engine on docstrings, allowing for jinja syntax inside docstrings. '''
+  _lines = jinja_render_string(app, "\n".join(lines))
+  _lines += "\n"
+  lines.clear()
+  lines += _lines.split("\n")
--- python/web/source/conf.py
+++ python/web/source/conf.py
@@ -10,9 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys, pathlib
-import sphinxbootstrap4theme
+import os, sys, pathlib
 import origen
 import origen.web
 
@@ -24,11 +22,8 @@ project = 'origen'
 copyright = '2020, Origen Core Team'
 author = 'Origen Core Team'
 
-
 # -- General configuration ---------------------------------------------------
 import rustdoc
-import recommonmark
-from recommonmark.transform import AutoStructify
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -37,15 +32,92 @@ extensions = [
   'origen.web.sphinx_ext.sphinx',
   'rustdoc',
   'sphinx.ext.autodoc',
-
-  # Causes a bunch of warnings with the APIs. Need to look into this first
-  # It works, but the build output is ugly
-  #'sphinx.ext.autosectionlabel',
-
+  'sphinx.ext.autosectionlabel',
   'sphinx.ext.inheritance_diagram',
   'autoapi.sphinx',
   'recommonmark',
+  'sphinx.ext.napoleon',
+  'sphinx.ext.extlinks'
 ]
+
+# Makes the references more verbose, but fixes a lot duplicated references resulting from autodoc.
+# Silver lining to the verbosity is they're also crystal clear as to where they're going.
+autosectionlabel_prefix_document = True
+
+extlinks = {
+  'sphinx_homepage': ('https://www.sphinx-doc.org/en/master/index.html%s', ''),
+  'sphinx_app': ('https://www.sphinx-doc.org/en/master/usage/quickstart.html#getting-started%s', ''),
+  'sphinx_docs': ('https://www.sphinx-doc.org/en/master/contents.html%s', ''),
+  'sphinx_extensions': ('https://www.sphinx-doc.org/en/master/usage/extensions/index.html%s', ''),
+  'sphinx_themes': ('https://www.sphinx-doc.org/en/master/usage/theming.html#themes%s', ''),
+  'sphinx_using_a_theme': ('https://www.sphinx-doc.org/en/master/usage/theming.html#using-a-theme%s', ''),
+  'sphinx_builtin_themes': ('https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes%s', ''),
+  'sphinx_add_theme': ('https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.add_html_theme%s', ''),
+  'sphinx_creating_themes': ('https://www.sphinx-doc.org/en/master/theming.html#creating-themes%s', ''),
+  'sphinx_available_themes': ('https://sphinx-themes.org/%s', ''),
+  'sphinx_project_examples': ('https://www.sphinx-doc.org/en/master/examples.html#projects-using-sphinx%s', ''),
+  'sphinx_conf': ('https://www.sphinx-doc.org/en/master/usage/configuration.html#module-conf%s', ''),
+  'sphinx_add_config_var': ('https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.add_config_value%s', ''),
+  'sphinx_confval_html_logo': ('https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_logo%s', ''),
+  'sphinx_confval_html_favicon': ('https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_favicon%s', ''),
+  'sphinx_confval_html_theme_options': ('https://www.sphinx-doc.org/en/3.x/usage/configuration.html#confval-html_theme_options%s', ''),
+  'sphinx_rst': ('https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html%s', ''),
+  'sphinx_rst_primer': ('https://www.sphinx-doc.org/en/master/usage/restructuredtext/basics.html%s', ''),
+  'sphinx_rst_directives': ('https://www.sphinx-doc.org/en/master/usage/restructuredtext/directives.html%s', ''),
+  'sphinx_templating': ('https://www.sphinx-doc.org/en/master/templating.html#templating%s', ''),
+  'sphinx_manpages': ('https://www.sphinx-doc.org/en/master/man/index.html%s', ''),
+  'sphinx_build_phases': ('https://www.sphinx-doc.org/en/master/extdev/index.html#build-phases%s', ''),
+  'sphinx_connect': ('https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.connect%s',''),
+  'sphinx_event_config_inited': ('https://www.sphinx-doc.org/en/master/extdev/appapi.html#event-config-inited%s', ''),
+  'sphinx_event_builder_inited': ('https://www.sphinx-doc.org/en/master/extdev/appapi.html#event-builder-inited%s', ''),
+  'sphinx_alabaster_theme': ('https://alabaster.readthedocs.io/en/latest/%s', ''),
+  'rst_quickstart': ('https://docutils.sourceforge.io/docs/user/rst/quickstart.html%s', ''),
+  'rst_cheatsheet': ('https://docutils.sourceforge.io/docs/user/rst/cheatsheet.txt%s', ''),
+  'rst_docs': ('https://docutils.sourceforge.io/rst.html%s', ''),
+  'rst_spec': ('https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html%s', ''),
+  'rst_cokelaer_cheatsheet': ('https://thomas-cokelaer.info/tutorials/sphinx/rest_syntax.html#contents-directives%s', ''),
+  'rst_guide_zephyr': ('https://docs.zephyrproject.org/latest/guides/documentation/index.html%s', ''),
+  'jinja_home': ('https://palletsprojects.com/p/jinja/%s', ''),
+  'jinja_docs': ('https://jinja.palletsprojects.com/en/master/%s', ''),
+  'recommonmark_home': ('https://recommonmark.readthedocs.io/en/latest/%s', ''),
+  'recommonmark_embedded_rst': ('https://recommonmark.readthedocs.io/en/latest/auto_structify.html#embed-restructuredtext%s', ''),
+  'markdown_home': ('https://www.markdownguide.org/%s', ''),
+  'autoapi_home': ('https://autoapi.readthedocs.io/%s', ''),
+  'autoapi_usage': ('https://autoapi.readthedocs.io/#usage%s', ''),
+  'autodoc_home': ('https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html%s', ''),
+  'bootstrap4': ('https://getbootstrap.com/docs/4.5/getting-started/introduction/%s', ''),
+  'bootstrap4_widgets': ('https://getbootstrap.com/docs/4.0/components/alerts/%s', ''),
+  'bootstrap4_sphinx_theme': ('http://myyasuda.github.io/sphinxbootstrap4theme/%s', ''),
+  'bootstrap4_sphinx_theme_options': ('http://myyasuda.github.io/sphinxbootstrap4theme/setup.html#html-theme-options%s', ''),
+  'bootstrap4_sphinx_theme_templates': ('https://github.com/myyasuda/sphinxbootstrap4theme/tree/master/themes/sphinxbootstrap4theme%s', ''),
+  'darkly': ('https://bootswatch.com/darkly/%s', ''),
+  'dracula_pygments': ('https://draculatheme.com/pygments%s', ''),
+  'o2_github_root': ('https://github.com/Origen-SDK/o2%s', ''),
+  'static_website': ('https://en.wikipedia.org/wiki/Static_web_page%s', ''),
+  'python_docs': ('https://docs.python.org/3.8/index.html%s', ''),
+}
+
+doc_root = "guides/documenting"
+pattern_api_root = 'guides/testers/pattern_generation/pattern_api'
+program_api_root = 'guides/testers/program_generation/program_api'
+utility_root = 'guides/runtime/utilities'
+origen_refs_for = {
+  'web_output_dir': f"{doc_root}/further_customizing_your_sphinx_app:Application Customizations",
+  'ose': f"{doc_root}/the_origen_sphinx_extension:The Origen Sphinx Extension",
+  'origen-s_sphinx_app': f"{doc_root}/your_sphinx_app:Origen's Sphinx App",
+  'ose_theme_opts': f"{doc_root}/the_origen_sphinx_extension:Origen Theme Options",
+  'ose_config_vars': f"{doc_root}/the_origen_sphinx_extension:Configuration Variables",
+  'documenting_introduction': f"{doc_root}/introduction:Introduction",
+  'documenting_core_concepts': f"{doc_root}/core_concepts:Core Concepts",
+  'api_generation': f"{doc_root}/your_sphinx_app:Automatic API Generation",
+  'ose_favicon': f"{doc_root}/the_origen_sphinx_extension:Favicon",
+  'ose_logos': f"{doc_root}/the_origen_sphinx_extension:Logos",
+  'ose_subprojects': f"{doc_root}/the_origen_sphinx_extension:SubProjects",
+  'origen_sphinx_app_extensions': f"{doc_root}/your_sphinx_app/Extensions",
+  'pattern_api_comments': f'{pattern_api_root}:Comments',
+  'program_api_comments': f'{program_api_root}:Comments',
+  'logger': f'{utility_root}/logger:Logger',
+}
 
 rustdoc_output_dir = origen.web.static_dir.joinpath('rustdoc')
 rustdoc_apply_svg_workarounds = True
@@ -63,12 +135,20 @@ rustdoc_projects = {
     'build_options': {'bin': 'origen'}
   }
 }
+
 autoapi_modules = {
-  'origen': None,
-  '_origen': {'orphan': True}
+  'origen': {
+    'module-members': ['undoc-members'],
+    'class-members': ['members', 'undoc-members', 'inherited-members']
+  },
+  '_origen': {'orphan': True,
+    'exclude-members': ['__doc__'],
+    'class-members': ['members', 'undoc-members', 'inherited-members']
+  }
 }
 autoapi_output_dir = origen.web.interbuild_dir.joinpath('autoapi')
-autodoc_default_flags = ['members', 'undoc-members', 'inherited-members']
+
+# Build the example project's docs into this one.
 origen_subprojects = {
   'example': {
     'source': origen.root.joinpath('../example'),
@@ -86,6 +166,7 @@ html_theme_options = {
     ('Example App', '_static/build/origen_sphinx_ext/example/sphinx_build/index', False)
   ],
 }
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -95,22 +176,23 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'origen'
-#html_theme = 'sphinxbootstrap4theme'
-#html_theme_path = [sphinxbootstrap4theme.get_path()]
-#html_logo = "_static/origen-device.png" #"dummy.png"
-#html_favicon =  None #"https://origen-sdk.org/favicon-32x32.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+origen_api_module_data_clashes = {
+  'origen': [
+    'origen.tester',
+    'origen.producer'
+  ]
+}
 
-# github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
-# def setup(app):
-#     app.add_config_value('recommonmark_config', {
-#             'url_resolver': lambda url: github_doc_root + url,
-#             'auto_toc_tree_section': 'Contents',
-#             }, True)
-#     app.add_transform(AutoStructify)
+def setup(app):
+  # Since origen.logger is actually a reference to a module, AutoAPI will skip it.
+  # Throw it back in as a variable.
+  def replace_origen_logger(app, node):
+    if node.name == 'origen':
+      node.variables['logger'] = (getattr(origen, 'logger'), node.default_variable_opts())
+  app.connect('autoapi-process-node', replace_origen_logger)
