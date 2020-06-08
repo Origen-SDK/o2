7ebb8c858fb1dec3fd7d1d35af3b4b495f319bf2
Stephen McGinty <stephen.mcginty@nxp.com>
2020-06-08 08:52:06 -0700

    dssc driver improvements

--- rust/origen/src/revision_control/designsync.rs
+++ rust/origen/src/revision_control/designsync.rs
@@ -29,22 +29,26 @@ impl RevisionControlAPI for Designsync {
         log_info!("Populating {}", &self.local.display());
         fs::create_dir_all(&self.local)?;
         self.set_vault()?;
-        self.pop(true, Some(Path::new(".")), version)
+        self.pop(true, Some(Path::new(".")), Some(version))
     }
 
     fn checkout(&self, force: bool, path: Option<&Path>, version: &str) -> Result<bool> {
-        self.pop(force, path, version)?;
+        self.pop(force, path, Some(version))?;
         Ok(false)
     }
 
-    fn revert(&self, path: Option<&Path>) -> Result<()> {
+    fn revert(&self, _path: Option<&Path>) -> Result<()> {
         // It seems like there is no simple way to checkout the current view without
         // potentially pulling in additional updates from the server.
         // This works by generating a throwaway tag to record the current versions of all
         // files, then forcing a re-pop of that tag.
         let timestamp = format!("ts{}", Utc::now().timestamp_millis());
         self._tag(&timestamp, false, None, true)?;
-        self.pop(true, None, &timestamp)?;
+        self.with_selector(&timestamp, || {
+            self.pop(true, None, None)?;
+            Ok(())
+        })?;
+        self.delete_tag(&timestamp)?;
         Ok(())
     }
 
@@ -129,6 +133,74 @@ impl Designsync {
         })
     }
 
+    /// Returns the current workspace selector
+    fn get_selector(&self) -> Result<String> {
+        let mut process = Command::new("dssc")
+            .args(&["url", "selector", &format!("{}", self.local.display())])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+
+        let mut selector = "".to_string();
+
+        log_stdout_and_stderr(&mut process, 
+            // The last line captured will contain the selector                  
+            Some(&mut |line: &str| {
+                log_debug!("{}", line);
+                selector = line.to_string();
+            }),
+            None
+        );
+
+        if process.wait()?.success() {
+            log_debug!("get_selector returned '{}'", &selector);
+            Ok(selector)
+        } else {
+            error!(
+                "Something went wrong setting the selector in '{}', see log for details",
+                self.local.display()
+            )
+        }
+    }
+
+    /// Execute the given function with the workspace selector set to the given value.
+    /// The workspace will be restored to its original selector at the end.
+    fn with_selector<T, F>(&self, selector: &str, mut f: F) -> Result<T>
+    where
+        F: FnMut() -> Result<T>,
+    {
+        let existing = self.get_selector()?;
+        let r = self.set_selector(selector);
+        if r.is_err() {
+            let _ = self.set_selector(&existing);
+            return error!("{:?}", r);
+        }
+        let result = f();
+        self.set_selector(&existing)?;
+        result
+    }
+
+    fn set_selector(&self, selector: &str) -> Result<()> {
+        log_debug!("Setting workspace selector to '{}'", selector);
+        let mut process = Command::new("dssc")
+            .args(&["setselector", selector, &format!("{}", self.local.display())])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+
+        log_stdout_and_stderr(&mut process, None, None);
+
+        if process.wait()?.success() {
+            Ok(())
+        } else {
+            Err(Error::new(&format!(
+                "Something went wrong setting the selector to '{}' in '{}', see log for details",
+                selector,
+                self.local.display()
+            )))
+        }
+    }
+
     fn _tag(&self, tagname: &str, force: bool, _message: Option<&str>, modified: bool) -> Result<()> {
         with_dir(&self.local, || {
             let replace = match force {
@@ -179,16 +251,41 @@ impl Designsync {
         })
     }
 
-    fn pop(&self, force: bool, path: Option<&Path>, version: &str) -> Result<()> {
-        let path = path.unwrap_or(Path::new("."));
+    fn delete_tag(&self, tagname: &str) -> Result<()> {
         with_dir(&self.local, || {
-            let mut args = vec!["pop", "-rec", "-uni", "-version"];
-            args.push(version);
+            let mut process = Command::new("dssc")
+                .args(&["tag", "-delete", "-rec", tagname, "."])
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()?;
+
+            log_stdout_and_stderr(&mut process, None, None);
+
+            let p = process.wait()?;
+
+            if p.success() {
+                Ok(())
+            } else {
+                error!(
+                    "Something went wrong deleting tag '{}', see log for details",
+                    tagname
+                )
+            }
+        })
+    }
+
+    fn pop(&self, force: bool, path: Option<&Path>, version: Option<&str>) -> Result<()> {
+        let path = path.unwrap_or(Path::new("."));
+        if let Some(version) = version {
+            self.set_selector(version)?;
+        }
+        with_dir(&self.local, || {
+            let mut args = vec!["pop", "-rec", "-uni"];
             if force {
                 args.push("-force");
+            } else {
+                args.push("-merge");
             }
-            //    args.push("-merge");
-            //}
             args.push(path.to_str().unwrap());
             log_debug!("Running DesignSync command: dssc {}", args.join(" "));
             let mut process = Command::new("dssc")
@@ -197,21 +294,9 @@ impl Designsync {
                 .stderr(Stdio::piped())
                 .spawn()?;
 
-            lazy_static! {
-                static ref POP_REGEX: Regex =
-                    Regex::new(r": Success - (Fetched|Created symbolic)").unwrap();
-            }
-
             log_stdout_and_stderr(
                 &mut process,
                 None,
-                // Example of a callback
-                //Some(&mut |line: &str| {
-                //    if POP_REGEX.is_match(&line) {
-                //        progress.received_objects += 1;
-                //        progress.completed_objects += 1;
-                //    }
-                //}),
                 None,
             );
 
