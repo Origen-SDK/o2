e1ac564c7652058ef2f4ca7daf1e221cca66d107
Corey Engelken <corey.engelken@nxp.com>
2020-07-23 10:29:11 -0500

    Setting target in Travis. Need a better long term solution though

--- ci/test.sh
+++ ci/test.sh
@@ -49,6 +49,7 @@ main() {
         ../rust/origen/target/$TARGET/debug/origen -v
         ../rust/origen/target/$TARGET/debug/origen setup
         $HOME/.poetry/bin/poetry run pytest -vv
+        ../rust/origen/target/$TARGET/debug/origen t s eagle_with_smt7
         ../rust/origen/target/$TARGET/debug/origen examples
         cd ../rust
     fi
