b72187a3f33ba134c56a36d76c1b4a23f4419235
Stephen McGinty <stephen.f.mcginty@gmail.com>
2020-09-15 15:08:10 +0100

    Attempt to upgrade pip version in CI

--- ci/install.sh
+++ ci/install.sh
@@ -21,9 +21,8 @@ main() {
         ln -s /home/travis/virtualenv/python$PYTHON_VERSION/bin/pip /home/travis/bin/pip
         ln -s /home/travis/virtualenv/python$PYTHON_VERSION/bin/pip3 /home/travis/bin/pip3
     fi
-    python --version
-
-    pip install --upgrade pip
+    python3 -m pip install --upgrade pip
+    python3 --version
 }
 
 main
