5409d86c86ddb9de8a45cbe4118c7fe13e582318
Corey Engelken <corey.engelken@nxp.com>
2020-09-30 14:09:56 -0500

    Remove debug line

--- rust/origen/cli/src/python.rs
+++ rust/origen/cli/src/python.rs
@@ -92,7 +92,6 @@ fn extract_version(text: &str) -> Option<Version> {
 
     match re.captures(text) {
         Some(x) => {
-            println!("{:?}", x);
             let c = x.get(1).unwrap().as_str();
             let v = Version::parse(c).unwrap();
             return Some(v);
