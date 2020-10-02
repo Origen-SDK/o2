60e8d00e43f28888319456472b500f16f441f22f
Stephen McGinty <stephen.f.mcginty@gmail.com>
2020-10-02 09:16:53 +0100

    More test template definitions

--- rust/origen/src/prog_gen/mod.rs
+++ rust/origen/src/prog_gen/mod.rs
@@ -3,8 +3,8 @@ mod teradyne;
 mod test_program;
 mod tests;
 
-use indexmap::IndexMap;
 use phf::phf_map;
+use std::collections::HashMap;
 pub use test_program::TestProgram;
 
 // This includes a map of all test template files, it is built by build.rs at compile time.
@@ -24,3 +24,45 @@ pub use test_program::TestProgram;
 // Doing it this way means that we can just drop new files into the templates dirs and they will
 // automatically be picked up and included in the new build.
 include!(concat!(env!("OUT_DIR"), "/test_templates.rs"));
+
+/// Test template definitions from json files are read into this structure
+#[derive(Debug, Deserialize)]
+struct TestTemplate {
+    parameter_list: Option<HashMap<String, String>>,
+    aliases: Option<HashMap<String, String>>,
+    values: Option<HashMap<String, serde_json::Value>>,
+    parameters: Option<HashMap<String, TestTemplateParameter>>,
+    class_name: Option<String>,
+    accepted_values: Option<HashMap<String, Vec<serde_json::Value>>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestTemplateParameter {
+    kind: Option<String>,
+    aliases: Option<Vec<String>>,
+    value: Option<serde_json::Value>,
+    accepted_values: Option<Vec<serde_json::Value>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestLibrary {
+    class_name_prefix: Option<String>,
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+
+    #[test]
+    fn test_all_templates_import_cleanly() {
+        for (file, content) in TEST_TEMPLATES.entries() {
+            if file.ends_with("library.json") {
+                let _: TestLibrary =
+                    serde_json::from_str(content).expect(&format!("Failed to import {}", file));
+            } else {
+                let _: TestTemplate =
+                    serde_json::from_str(content).expect(&format!("Failed to import {}", file));
+            }
+        }
+    }
+}
