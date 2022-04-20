ee05ed1a272bac26b90d211073a9cc4161dc607b
Stephen McGinty <stephen.f.mcginty@gmail.com>
2022-04-20 16:39:26 +0100

    Update Poetry and fix warnings with latest Rust

--- rust/origen/src/prog_gen/model/sub_test.rs
+++ rust/origen/src/prog_gen/model/sub_test.rs
@@ -3,11 +3,16 @@ use super::Limit;
 /// SubTests are used to model a test method or IG-XL flow line which has multiple limits.
 #[derive(Debug)]
 pub struct SubTest {
+    #[allow(dead_code)]
     test_id: usize,
     /// If not present the name will be derived from the parent test
+    #[allow(dead_code)]
     name: Option<String>,
     /// Optional test number
+    #[allow(dead_code)]
     number: Option<usize>,
+    #[allow(dead_code)]
     lo_limit: Option<Limit>,
+    #[allow(dead_code)]
     hi_limit: Option<Limit>,
 }
