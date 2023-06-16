40839dcaaf7a8c229e17e5094f37e81bf96d998d
Stephen McGinty <stephen.mcginty@amd.com>
2023-06-16 13:15:28 +0100

    Added support for STIL load path and relaxed STIL grammar slightly

--- rust/origen_metal/src/stil/mod.rs
+++ rust/origen_metal/src/stil/mod.rs
@@ -6,20 +6,36 @@ pub mod processors;
 use crate::ast::Node;
 use crate::Result as OrigenResult;
 pub use nodes::STIL;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub fn from_file(path: &Path) -> OrigenResult<Node<STIL>> {
     let ast = parser::parse_file(path)?;
-    let ast = processors::includer::Includer::run(&ast, Path::new(path).parent())?;
+    let load_path = vec![Path::new(path).parent().unwrap().to_path_buf()];
+
+    let ast = processors::includer::Includer::run(&ast, load_path)?;
+    Ok(ast)
+}
+
+pub fn from_file_with_load_path(path: &Path, load_path: &Vec<PathBuf>) -> OrigenResult<Node<STIL>> {
+    let ast = parser::parse_file(path)?;
+    let mut load_path_with_current = vec![Path::new(path).parent().unwrap().to_path_buf()];
+    for p in load_path {
+        load_path_with_current.push(p.clone());
+    }
+    let ast = processors::includer::Includer::run(&ast, load_path_with_current)?;
     Ok(ast)
 }
 
 pub fn from_str(stil: &str, root_dir: Option<&str>) -> OrigenResult<Node<STIL>> {
     let ast = parser::parse_str(stil)?;
-    let ast = match root_dir {
-        Some(p) => processors::includer::Includer::run(&ast, Some(Path::new(p)))?,
-        None => processors::includer::Includer::run(&ast, None)?,
+    let load_path = {
+        if let Some(p) = root_dir {
+            vec![Path::new(p).to_path_buf()]
+        } else {
+            vec![]
+        }
     };
+    let ast = processors::includer::Includer::run(&ast, load_path)?;
     Ok(ast)
 }
 #[derive(Clone, Debug, PartialEq, Serialize, enum_utils::FromStr)]
--- rust/origen_metal/src/stil/processors/includer.rs
+++ rust/origen_metal/src/stil/processors/includer.rs
@@ -7,20 +7,21 @@ use crate::ast::{Processor, Return};
 use crate::Result;
 use shellexpand;
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 pub struct Includer {
-    dir: PathBuf,
+    load_path: Vec<PathBuf>,
 }
 
 impl Includer {
     #[allow(dead_code)]
-    pub fn run(node: &Node<STIL>, dir: Option<&Path>) -> Result<Node<STIL>> {
+    pub fn run(node: &Node<STIL>, load_path: Vec<PathBuf>) -> Result<Node<STIL>> {
+        let mut full_load_path = vec![env::current_dir()?];
+        for p in load_path {
+            full_load_path.push(p.to_path_buf());
+        }
         let mut p = Includer {
-            dir: match dir {
-                Some(p) => p.to_path_buf(),
-                None => env::current_dir()?,
-            },
+            load_path: full_load_path,
         };
         Ok(node.process(&mut p)?.unwrap())
     }
@@ -28,20 +29,27 @@ impl Includer {
 
 impl Processor<STIL> for Includer {
     fn on_node(&mut self, node: &Node<STIL>) -> Result<Return<STIL>> {
-        let result = match &node.attrs {
+        match &node.attrs {
             STIL::Include(file, _) => {
-                let expanded = format!("{}", shellexpand::full(file)?);
-                let mut path = self.dir.clone();
-                // Note that if expanded is absolute then the push method will replace the current
-                // path with the given one
-                path.push(expanded);
-                let ast = parser::parse_file(&path)?;
-                Return::Replace(Includer::run(&ast, path.parent())?)
+                let expanded = format!("{}", shellexpand::full(&file)?);
+                for p in &self.load_path {
+                    let mut path = p.clone();
+                    // Note that if expanded is absolute then the push method will replace the current
+                    // path with the given one
+                    path.push(&expanded);
+                    if path.exists() {
+                        let ast = parser::parse_file(&path)?;
+                        return Ok(Return::Replace(Includer::run(
+                            &ast,
+                            self.load_path.clone(),
+                        )?));
+                    }
+                }
+                bail!("Unable to find include file: {}", expanded)
             }
-            STIL::Root => Return::ProcessChildren,
+            STIL::Root => Ok(Return::ProcessChildren),
             // No need to recurse into other nodes, all includes should be at the top-level
-            _ => Return::Unmodified,
-        };
-        Ok(result)
+            _ => Ok(Return::Unmodified),
+        }
     }
 }
