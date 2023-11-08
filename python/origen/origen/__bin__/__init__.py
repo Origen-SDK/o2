17f0dcac722294d3bb8375f6c02a400be0bdadfc
coreyeng <coreyengelken@gmail.com>
2023-11-08 13:17:17 -0600

    Debug: Try reverting run cmd for Linux.

--- python/origen/origen/__bin__/__init__.py
+++ python/origen/origen/__bin__/__init__.py
@@ -5,7 +5,13 @@ import platform
 on_windows = platform.system() == 'Windows'
 
 def run_origen():
-    subprocess.run([
-        str(pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath("origen")),
-        *sys.argv[1:]
-    ], shell=on_windows)
+    if on_windows:
+        subprocess.run([
+            str(pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath("origen")),
+            *sys.argv[1:]
+        ], shell=on_windows)
+    else:
+        subprocess.run(str(
+            pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath(
+                "origen")) + " " + " ".join(sys.argv[1:]),
+                    shell=True)
