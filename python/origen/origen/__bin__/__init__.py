bed413e6f9206fa718396e1d983e7db7e7d693b7
coreyeng <coreyengelken@gmail.com>
2023-11-07 19:43:11 -0600

    Debug: Robustness/usage fixes for 'run_origen' poetry wrapper

--- python/origen/origen/__bin__/__init__.py
+++ python/origen/origen/__bin__/__init__.py
@@ -6,6 +6,6 @@ on_windows = platform.system() == 'Windows'
 
 def run_origen():
     subprocess.run([
-        pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath("origen"),
+        str(pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath("origen")),
         *sys.argv[1:]
     ], shell=on_windows)
