0def459acbf086d5d3b696edb65cff7e3a6b3063
coreyeng <coreyengelken@gmail.com>
2023-11-09 08:09:35 -0600

    Retry robustness update in run_origen

--- python/origen/origen/__bin__/__init__.py
+++ python/origen/origen/__bin__/__init__.py
@@ -5,13 +5,7 @@ import platform
 on_windows = platform.system() == 'Windows'
 
 def run_origen():
-    if on_windows:
-        subprocess.run([
-            str(pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath("origen")),
-            *sys.argv[1:]
-        ], shell=on_windows)
-    else:
-        subprocess.run(str(
-            pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath(
-                "origen")) + " " + " ".join(sys.argv[1:]),
-                    shell=True)
+    subprocess.run([
+        str(pathlib.Path(__file__).parent.absolute().joinpath("bin").joinpath("origen")),
+        *sys.argv[1:]
+    ], shell=on_windows)
